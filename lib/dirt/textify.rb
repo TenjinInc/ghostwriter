49167b705ccf3e32173867e76104bb8e61e157d6
Robin Miller <robin@tenjin.ca>
2015-12-17 17:57:18 -0700

    Wrote tests and completed basic textification

--- lib/dirt/textify.rb
+++ lib/dirt/textify.rb
@@ -1,7 +1,34 @@
-require "dirt/textify/version"
+require 'dirt/textify/version'
+require 'nokogiri'
 
 module Dirt
   module Textify
-    # Your code goes here...
+    # Transmutes HTML into text.
+    def self.textify(html)
+      html.gsub!(/\s+/, ' ')
+      html.gsub!('</p>', "</p>\n\n")
+
+      doc = Nokogiri::HTML(html)
+
+      doc.search('style').remove
+
+      doc.search('a').each do |link_node|
+        link_node.inner_html = link_node.inner_html + ' (' + link_node['href'] + ")\n"
+      end
+
+      doc.search('hr').each do |link_node|
+        link_node.replace '----------'
+      end
+
+      doc.search('br').each do |link_node|
+        link_node.replace "\n"
+      end
+
+      # doc.search('p').each do |link_node|
+      #   link_node.inner_html = link_node.inner_html + "\n\n"
+      # end
+
+      doc.text
+    end
   end
 end
